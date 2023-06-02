e75815ac36559916e6ec0b9e2f63101ebd9687d5
Diego Oruna <diego.orunac@gmail.com>
2023-06-02 14:19:26 -0500

    rename package

---
+++ Tests/LiquissetTests/LiquissetTests.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Diego Oruna on 2/06/23.
+//
+
+import XCTest
+@testable import Liquisset
+
+final class LiquissetTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+    }
+}
