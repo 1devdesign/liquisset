4cacf2d1578e320d5c6f9c28aec88b172ff4b641
Diego Oruna <diego.orunac@gmail.com>
2023-06-02 10:21:26 -0500

    init package

---
+++ README.md
@@ -0,0 +1,3 @@
+# liquisset
+
+A description of this package.
---
+++ Tests/liquissetTests/liquissetTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import liquisset
+
+final class liquissetTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(liquisset().text, "Hello, World!")
+    }
+}
